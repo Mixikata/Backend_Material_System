308b2948809c9b5eb8b51c2c4ff3cdf949b48cd3
cmy <3259496351@qq.com>
2025-04-28 17:35:28 +0800

    功能完善

--- src/main/java/org/example/backend_material_system/config/WebMvcConfig.java
+++ src/main/java/org/example/backend_material_system/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package org.example.backend_material_system.config;
 
+import org.example.backend_material_system.common.JacksonObjectMapper;
 import org.example.backend_material_system.common.MyInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -12,5 +17,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(new MyInterceptor())
                 .addPathPatterns("/")          // 拦截路径
                 .excludePathPatterns("/login");  // 排除路径
+    }
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        converters.add(0,messageConverter);
     }
 }
